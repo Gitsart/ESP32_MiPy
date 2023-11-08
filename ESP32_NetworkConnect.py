b75f9d2da43a0b6581dd5d0be4ba6321c2b06391
Gitsart <142268471+Gitsart@users.noreply.github.com>
2023-11-08 17:26:18 +0530

    Add files via upload

---
+++ ESP32_NetworkConnect.py
@@ -0,0 +1,17 @@
+import network
+
+def connect_to_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)  # Create a station interface object
+    if not wlan.isconnected():  # Check if it's already connected
+        print('Connecting to WiFi...')
+        wlan.active(True)  # Activate the interface
+        wlan.connect(ssid, password)  # Connect to the WiFi network
+
+        while not wlan.isconnected():
+            pass
+
+    print('Connected to WiFi')
+    print('Network config:', wlan.ifconfig())
+
+# Replace 'your_SSID' and 'your_password' with your actual WiFi credentials
+connect_to_wifi('Vaishak M', '03101997')
